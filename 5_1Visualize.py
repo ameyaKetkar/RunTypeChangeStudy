e47e7aaa00fd7790110a5487483aceedb954668e
Ameya Ketkar <t-amketk@microsoft.com>
2020-06-03 12:45:57 -0700

    Add file for running python with python

---
+++ 5_1Visualize.py
@@ -0,0 +1,35 @@
+import subprocess
+import os
+
+
+
+
+def execute(cmd, cwd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, cwd=cwd)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+
+
+projectPath = os.path.dirname(os.path.abspath(__file__))
+
+from configparser import ConfigParser
+config = ConfigParser()
+
+config.read(os.path.join(projectPath,'config.ini'))
+
+setupPath = config.get('main','path')
+
+dataAnalysis = os.path.join(os.path.join(os.path.normpath(setupPath), 'TypeChangeStudy'), 'DataAnalysis')
+os.chdir(dataAnalysis)
+
+# cmd = ['python', 'ToHtml.py']
+
+os.system('python ToHtml.py')
+
+# for result in execute(cmd, str(dataAnalysis)):
+  #  print(result)
