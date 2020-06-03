da36a39430c14625dc52d19ec3bf7abd8247a77c
ChangeType <javatypechange@gmail.com>
2020-06-03 11:15:37 -0700

    Updating runner

--- 1CollectDepsAndTypeChanges.py
+++ 1CollectDepsAndTypeChanges.py
@@ -15,17 +15,29 @@ def runLongCommand(s, cwd =''):
         o, e = out.communicate()
         return e, o
 
+def execute(cmd, cwd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, cwd=cwd)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+
 def run_command(command, cwd = ''):
     if cwd == '' :
         p = subprocess.Popen(command,
                      stdout=subprocess.PIPE,
                      stderr=subprocess.STDOUT)
+        p.wait()
         return iter(p.stdout.readline, b'')
     else:
         p = subprocess.Popen(command,
                      stdout=subprocess.PIPE,
                      stderr=subprocess.STDOUT,
                      cwd =cwd)
+        p.wait()
         return iter(p.stdout.readline, b'')
 
 
@@ -44,7 +56,7 @@ os.chdir(simpleTypechangeMiner)
 
 cmd = ['java', '-cp', 'lib/*', 'Runner']
 
-result = run_command(cmd, cwd = str(simpleTypechangeMiner))
+result = execute(cmd, cwd = str(simpleTypechangeMiner))
 
 
 
